5467658478f5cd188c7b64d406351d6163ea1f44
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-11-11 15:35:47 +0100

    aggiunto testo e img tramite vue

---
+++ main.js
@@ -0,0 +1,10 @@
+const {createApp}= Vue
+
+createApp({
+    data(){
+        return{
+            text: "prova", 
+            img: "https://cdn.mos.cms.futurecdn.net/CAZ6JXi6huSuN4QGE627NR.jpg"
+        }
+    }
+}).mount("#app")
